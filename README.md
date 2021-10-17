272b564a40f2f8d4afbffe69618c3d3ffb7ee4db
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-10-18 00:36:46 +0100

    added serde_json example
    slightly better error feedback

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Send a json request to actix, and parse it.
 
 Note, this does not provide any helpful feedback to the client (for example, when an invalid or incomplete json payload is sent, there's no helpful error message like fastapi).
 
+To get slightly developer friendly error messages, such as:
+
+```
+curl -v -H "Content-Type: application/json" http://127.0.0.1:8080 -d '{"name": "fred"}'
+...
+missing field `number` at line 1 column 16(base)
+```
+
+Which is slightly more helpful, the `serde_json` create provides the above more useful feedback (though not as good fastapi in combination with Pydantic). TODO: research similar way of getting as good feedback with actix/serde.
+
 See docs: https://actix.rs/docs/request/ "JSON Request"
 
 See also crate `serde_json`.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use actix_web::{web, App, HttpServer, Result};
-use serde::Deserialize;
+use actix_web::{error, post, web, App, Error, HttpResponse, HttpServer};
+use futures::StreamExt;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
-struct Info {
-    username: String,
+#[derive(Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
 }
 
-/// extract `Info` using serde
-async fn index(info: web::Json<Info>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.username))
+const MAX_SIZE: usize = 262_144; // max payload size is 256k
+
+#[post("/")]
+async fn index_manual(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+    // payload is a stream of Bytes objects
+    let mut body = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+        // limit max size of in-memory payload
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(error::ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk);
+    }
+
+    // body is loaded, now we can deserialize serde-json
+    let obj = serde_json::from_slice::<MyObj>(&body)?;
+    Ok(HttpResponse::Ok().json(obj)) // <- send response
 }
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::post().to(index)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+
+    HttpServer::new(|| {
+			App::new()
+				.service(index_manual)
+		})
+		.bind("127.0.0.1:8080")?
+		.run()
+		.await
 }
